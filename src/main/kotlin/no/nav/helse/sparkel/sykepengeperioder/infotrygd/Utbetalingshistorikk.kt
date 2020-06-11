f655213f04461dd9d96b5861575edbe38c500f51
David Steinsland <david@steinsland.email>
2020-06-11 12:51:42 +0200

    runder ikke av inntekt
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

--- src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/infotrygd/Utbetalingshistorikk.kt
+++ src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/infotrygd/Utbetalingshistorikk.kt
@@ -2,8 +2,6 @@ package no.nav.helse.sparkel.sykepengeperioder.infotrygd
 
 import com.fasterxml.jackson.databind.JsonNode
 import org.slf4j.LoggerFactory
-import java.math.BigDecimal
-import java.math.RoundingMode
 import java.time.LocalDate
 
 class Utbetalingshistorikk(jsonNode: JsonNode) {
@@ -48,10 +46,10 @@ data class Utbetaling(
 
 data class Inntektsopplysninger(private val jsonNode: JsonNode) {
     private val periodeKode = PeriodeKode.verdiFraKode(jsonNode["periodeKode"].textValue())
-    private val lønn = jsonNode["loenn"].decimalValue()
+    private val lønn = jsonNode["loenn"].doubleValue()
 
     val sykepengerFom: LocalDate = LocalDate.parse(jsonNode["sykepengerFom"].textValue())
-    val inntekt: Int = periodeKode.omregn(lønn)
+    val inntekt: Double = periodeKode.omregn(lønn)
     val orgnummer: String = jsonNode["orgNr"].textValue()
     val refusjonTom: LocalDate? =
         jsonNode["refusjonTom"].takeUnless { it.isNull }?.let { LocalDate.parse(it.textValue()) }
@@ -60,18 +58,18 @@ data class Inntektsopplysninger(private val jsonNode: JsonNode) {
     internal fun skalTilSpleis() = periodeKode != PeriodeKode.Premiegrunnlag
 
     internal enum class PeriodeKode(
-        val fraksjon: BigDecimal,
+        val fraksjon: Double,
         val kode: String
     ) {
-        Daglig(260.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "D"),
-        Ukentlig(52.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "U"),
-        Biukentlig(26.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "F"),
-        Månedlig(1.0.toBigDecimal().setScale(10), "M"),
-        Årlig(1.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "Å"),
-        SkjønnsmessigFastsatt(1.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "X"),
-        Premiegrunnlag(1.0.toBigDecimal().setScale(10) / 12.0.toBigDecimal(), "Y");
+        Daglig(260.0 / 12.0, "D"),
+        Ukentlig(52.0 / 12.0, "U"),
+        Biukentlig(26.0 / 12.0, "F"),
+        Månedlig(1.0, "M"),
+        Årlig(1.0 / 12.0, "Å"),
+        SkjønnsmessigFastsatt(1.0 / 12.0, "X"),
+        Premiegrunnlag(1.0 / 12.0, "Y");
 
-        fun omregn(lønn: BigDecimal): Int = (lønn * fraksjon).setScale(0, RoundingMode.HALF_UP).toInt()
+        fun omregn(lønn: Double): Double = (lønn * fraksjon)
 
         companion object {
             fun verdiFraKode(kode: String): PeriodeKode {
--- src/test/kotlin/no/nav/helse/sparkel/sykepengeperioder/infotrygd/InntektTest.kt
+++ src/test/kotlin/no/nav/helse/sparkel/sykepengeperioder/infotrygd/InntektTest.kt
@@ -1,55 +1,33 @@
 package no.nav.helse.sparkel.sykepengeperioder.infotrygd
 
+import no.nav.helse.sparkel.sykepengeperioder.infotrygd.Inntektsopplysninger.PeriodeKode.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.math.roundToInt
 
 class InntektTest {
 
     @Test
     fun `kan regne om daglig inntekt`() {
-        assertEquals(10833, Inntektsopplysninger.PeriodeKode.Daglig.omregn(500.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om ukentlig inntekt`() {
-        assertEquals(21667, Inntektsopplysninger.PeriodeKode.Ukentlig.omregn(5000.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om biukentlig inntekt`() {
-        assertEquals(21667, Inntektsopplysninger.PeriodeKode.Biukentlig.omregn(10000.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om måndelig inntekt`() {
-        assertEquals(39000, Inntektsopplysninger.PeriodeKode.Månedlig.omregn(39000.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om årlig inntekt`() {
-        assertEquals(43333, Inntektsopplysninger.PeriodeKode.Årlig.omregn(520000.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om skjønnsmessig fastsatt inntekt`() {
-        assertEquals(32083, Inntektsopplysninger.PeriodeKode.SkjønnsmessigFastsatt.omregn(385000.toBigDecimal()))
-    }
-
-    @Test
-    fun `kan regne om premiegrunnlag`() {
-        assertEquals(34667, Inntektsopplysninger.PeriodeKode.Premiegrunnlag.omregn(416000.toBigDecimal()))
-    }
-
-    @Test
-    fun `avrunding etter half-up prinsippet`() {
-        assertEquals(41667, Inntektsopplysninger.PeriodeKode.Årlig.omregn(500000.toBigDecimal()))
-        assertEquals(26017, Inntektsopplysninger.PeriodeKode.Ukentlig.omregn(6004.toBigDecimal()))
-        assertEquals(26745, Inntektsopplysninger.PeriodeKode.Daglig.omregn(1234.4.toBigDecimal()))
+        assertInntekt(10833.33, 500.0, Daglig)
+        assertInntekt(21666.67, 5000.0, Ukentlig)
+        assertInntekt(21666.67, 10000.0, Biukentlig)
+        assertInntekt(39000.00, 39000.0, Månedlig)
+        assertInntekt(43333.33, 520000.0, Årlig)
+        assertInntekt(32083.33, 385000.0, SkjønnsmessigFastsatt)
+        assertInntekt(34666.67, 416000.0, Premiegrunnlag)
+        assertInntekt(41666.67, 500000.0, Årlig)
+        assertInntekt(26017.33, 6004.0, Ukentlig)
+        assertInntekt(26745.33, 1234.4, Daglig)
     }
 
     @Test
     fun `kaster feil ved andre periodetyper`() {
         assertThrows<IllegalArgumentException> { Inntektsopplysninger.PeriodeKode.verdiFraKode("I") }
+    }
+
+    private fun assertInntekt(expected: Double, lønn: Double, kode: Inntektsopplysninger.PeriodeKode) {
+        assertEquals(expected, (kode.omregn(lønn) * 100).roundToInt() / 100.0)
     }
 }
